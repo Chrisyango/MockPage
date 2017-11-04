63b952272cd5a946239bc12ffeca97afe0949808
Chrisyango <cyang287@yahoo.com>
2017-11-04 13:25:47 -0400

    Added a lot of content

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
 	
+	var nav = $(".nav");
+	var sec1 = $(".section1");
+
+	$(window).scroll(function() {
+		var height = window.innerHeight;
+		var vh = height * 0.92;
+		if($(this).scrollTop() > vh) {
+			nav.addClass("nav-top");
+			sec1.addClass("nav-section");
+		} else {
+			nav.removeClass("nav-top");
+			sec1.removeClass("nav-section");
+		}
+	});
+
 });
