d8d64df699b4448cfc860772c4294181afb116a1
Chrisyango <cyang287@yahoo.com>
2017-11-05 18:06:21 -0500

    Added media queries and added smooth scrolling

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,32 @@
 $(document).ready(function() {
 	
+
+	// Navigation stick to top after scroll
 	var nav = $(".nav");
 	var sec1 = $(".section1");
 
 	$(window).scroll(function() {
+
+		// Convert pixels into veiwport height
 		var height = window.innerHeight;
 		var vh = height * 0.92;
+
+		// Makes Navigation stick to top after scrolling past it
 		if($(this).scrollTop() > vh) {
 			nav.addClass("nav-top");
-			sec1.addClass("nav-section");
 		} else {
 			nav.removeClass("nav-top");
-			sec1.removeClass("nav-section");
 		}
+	});
+
+	// Smooth scrolling
+	$("a").click(function() {
+		var top = $("body").find($(this).attr("href")).offset().top;
+		$("html, body").animate({
+			scrollTop: top
+		}, 1000);
+
+		return false;
 	});
 
 });
